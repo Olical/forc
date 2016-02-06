475755e0589e761e97a8a87066158b7bde3958cc
Oliver Caldwell <olliec87@gmail.com>
2016-02-06 02:12:27 +0000

    Better name for forc first argument

--- src/forc.js
+++ src/forc.js
@@ -1,8 +1,8 @@
 import pairs from './utils/pairs'
 import {isIterable, isFunction} from './utils/typeChecks'
 
-export default function * forc (bindings, body) {
-  if (!isIterable(bindings)) {
+export default function * forc (seq, body) {
+  if (!isIterable(seq)) {
     throw new Error('The first argument must be iterable')
   }
 
@@ -10,7 +10,7 @@ export default function * forc (bindings, body) {
     throw new Error('The second argument must be a function')
   }
 
-  for (const [key, value] of pairs(bindings)) {
+  for (const [key, value] of pairs(seq)) {
     const iter = isFunction(value) ? value(/* bindings... */) : value
 
     for (const item of iter) {
