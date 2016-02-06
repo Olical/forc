810b0933591d415de1478db92965bdde7e519516
Oliver Caldwell <olliec87@gmail.com>
2016-02-06 15:14:08 +0000

    Fix test of multiple expressions

--- test/forc.js
+++ test/forc.js
@@ -42,7 +42,7 @@ test('multiple expressions', t => {
     'x', ['a', 'b', 'c'],
     'y', [1, 2, 3]
   ], ({x, y}) => [x, y])
-  t.deepEqual([...actual], [['a', 1], ['b', 2], ['c', 3]], 'values were zipped up')
+  t.deepEqual([...actual], [['a', 1], ['a', 2], ['a', 3], ['b', 1], ['b', 2], ['b', 3], ['c', 1], ['c', 2], ['c', 3]], 'values were zipped up')
 })
 
 test('using previous expressions', t => {
